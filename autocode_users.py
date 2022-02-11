19d1b63419d80af437133f924834a2d262d2813c
Albert Santos <alberteinsteincds@gmail.com>
2022-02-11 01:27:23 -0300

    função gerar txt posições dos usuários

--- autocode_users.py
+++ autocode_users.py
@@ -1,56 +1,36 @@
 import pandas as pd
 import numpy as np
 
-with open("user_position.txt",'w') as f: # Limpando  o arquivo
-    pass
+def autocode_users(planilha_users):
+    with open("user_position.txt",'w') as f: # Limpando  o arquivo
+        pass
 
-user_position = 'UserPosition.xls'
-small_position = 'SmallPosition.xls'
+    user_position = planilha_users
 
-df_usuarios = pd.read_excel(user_position)
-df_small = pd.read_excel(small_position)
+    df_usuarios = pd.read_excel(user_position)
 
-print(df_small)
-print(df_usuarios)
+    print(df_usuarios)
 
-user_matrix = df_usuarios.to_numpy()
-small_matrix = df_small.to_numpy()
-print(small_matrix)
-print(user_matrix.shape)
+    user_matrix = df_usuarios.to_numpy()
+    print(user_matrix.shape)
 
-hora = 1.0
-for posicao in range(np.size(user_matrix, 1)):  # Eixos x e y para cada hora
-    if posicao%2 != 0  and posicao!= 0 :
-        print(f'Hora {hora}')
-        with open('user_position.txt', 'a') as arquivo:
-                arquivo.write(f'\nINICIO HORA {hora}\n')
-        
-        for usuario in range(np.size(user_matrix, 0)):  # Quantidade de usuarios    
+    hora = 1.0
+    for posicao in range(np.size(user_matrix, 1)):  # Eixos x e y para cada hora
+        if posicao%2 != 0  and posicao!= 0 :
+            print(f'Hora {hora}')
+            with open('user_position.txt', 'a') as arquivo:
+                    arquivo.write(f'\nINICIO HORA {hora}\n')
             
-            if user_matrix[usuario, (posicao - 1)] != 0 and user_matrix[usuario,posicao] != 0:
-                print(f'    positionAlloc4->Add (Vector ({user_matrix[usuario, (posicao - 1)]},{user_matrix[usuario, posicao]}));')
-                #positionAlloc4->Add (Vector ( 406, 32, 0));
-                with open('user_position.txt', 'a') as arquivo:
-                    arquivo.write(f'    positionAlloc4->Add (Vector({user_matrix[usuario, (posicao - 1)]},{user_matrix[usuario, posicao]}));\n')
+            for usuario in range(np.size(user_matrix, 0)):  # Quantidade de usuarios    
+                
+                if user_matrix[usuario, (posicao - 1)] != 0 and user_matrix[usuario,posicao] != 0:
+                    print(f'    positionAlloc4->Add (Vector ({user_matrix[usuario, (posicao - 1)]},{user_matrix[usuario, posicao]}));')
+                    #positionAlloc4->Add (Vector ( 406, 32, 0));
+                    with open('user_position.txt', 'a') as arquivo:
+                        arquivo.write(f'    positionAlloc4->Add (Vector({user_matrix[usuario, (posicao - 1)]},{user_matrix[usuario, posicao]}));\n')
 
-        with open('user_position.txt', 'a') as arquivo:
-                arquivo.write(f'FIM HORA {hora}\n')
-        hora += 1
-    
+            with open('user_position.txt', 'a') as arquivo:
+                    arquivo.write(f'FIM HORA {hora}\n')
+            hora += 1
 
-
-        
-
-
-# print(user_matrix[29, 10])
-# print(np.size(user_matrix, 0))
-
-# for usuario in range(np.size(user_matrix, 0)):
-#     print(usuario)
-#     if usuario%2 != 0  and usuario!= 0 :
-#         print('True')
-
-
-
-
-
+autocode_users('UserPosition.xls')
