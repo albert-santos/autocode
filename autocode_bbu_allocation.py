234ff6a09eaa50092e951f3bc6b414bfd5a6276b
Albert Santos <alberteinsteincds@gmail.com>
2022-08-17 23:18:05 -0300

    add status rrh no autocode

--- autocode_bbu_allocation.py
+++ autocode_bbu_allocation.py
@@ -11,25 +11,33 @@ def autocode_bbu_allocation(planilha_bbu_allocation, planilha_rrhs_status):
 
     bbu_allocation = planilha_bbu_allocation
     df_bbu_allocation = pd.read_excel(bbu_allocation, header = None)
-
-    rrhs_status = planilha_rrhs_status
-    df_planilha_rrhs_status = pd.read_excel(rrhs_status, header=None)
-
     bbu_allocation_matrix = df_bbu_allocation.to_numpy()
     print(bbu_allocation_matrix.shape)
     # print(np.size(bbu_allocation_matrix, 1))
 
+    rrhs_status = planilha_rrhs_status
+    df_planilha_rrhs_status = pd.read_excel(rrhs_status, header=None)
+    rrh_status_matrix = df_planilha_rrhs_status.to_numpy()
+    print(f'Shape do vetor de status das RRHs {rrh_status_matrix.shape}')
+
     
-    for posicao in range(1, np.size(bbu_allocation_matrix, 0) + 1): # Número de linhas que representa as horas
+
+    
+    for hora in range(1, np.size(bbu_allocation_matrix, 0) + 1): # Número de linhas que representa as horas
         
       
         print(f'Hora {hora}')
         with open(f"{arquivo_txt}.txt", 'a') as arquivo:
                 arquivo.write(f'\nINICIO HORA {hora}\n')
         
-        # Número total de RRHs
-        for usuario in range(np.size(bbu_allocation_matrix, 1)):    
-            print('continue')
+        # Percorre todas RRHs
+        for rrh_indice in range(np.size(bbu_allocation_matrix, 1)):  
+
+            status = rrh_status_matrix[hora-1, rrh_indice]
+            if   status == 1:
+                print(f'continue: {bbu_allocation_matrix[hora-1, rrh_indice]}')
+
+                # Implementar código que vai para o NS-3 ------------
             
         with open(f"{arquivo_txt}.txt", 'a') as arquivo:
                     arquivo.write(f'FIM HORA {hora}\n')
@@ -38,4 +46,4 @@ def autocode_bbu_allocation(planilha_bbu_allocation, planilha_rrhs_status):
 
 
 
-autocode_bbu_allocation('ECC_planilhas/mapping_rrh_bbu_sectors_with_JasmineModel.xls')
+autocode_bbu_allocation('ECC_planilhas/mapping_rrh_bbu_sectors_with_JasmineModel.xls', 'ECC_planilhas/rrhs_status_with_JasmineModel.xls')
